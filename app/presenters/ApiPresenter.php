fbaeb38be82b75b444d98d38d985ba41f23d02cb
Long Do Hai <me@thinky.cz>
2015-08-27 00:19:55 +0200

    The Beginning

---
+++ app/presenters/ApiPresenter.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thinky
+ * Date: 25.06.15
+ * Time: 17:59
+ */
+
+namespace App\Presenters;
+
+
+use Nette\Application\UI\Presenter;
+
+class ApiPresenter extends Presenter{
+
+    public function renderDefault()
+    {
+        
+    }
+
+}
